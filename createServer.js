3a07710c12bea0392c5a3d3bf79cee1cd3c19ad2
ltragiang26 <158532853+ltragiang26@users.noreply.github.com>
2024-04-17 09:54:22 +0700

    Add files via upload

---
+++ createServer.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var fs = require('fs')
+
+http
+    .createServer(function (req,res) {
+        const fileStream = fs.createReadStream('./content/output.txt', 'utf8')
+        fileStream.on('open', () => {
+            fileStream.pipe(res)
+        })
+        fileStream.on('error', (err) => {
+            res.end(err)
+        })
+    })
+
+    .listen(5000)
